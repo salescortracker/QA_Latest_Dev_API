f52de82c5ecdaa4b8345764c4df9f0941a7bc767
Ravichandra2000 <ravichandra@cortracker360.com>
2026-02-17 17:51:17 +0530

    PerformanceReciewKPI's
    PerformanceReciewKPI's

--- HRMS_Backend/Program.cs
+++ HRMS_Backend/Program.cs
@@ -70,6 +70,8 @@ builder.Services.AddScoped<IMissedPunchService,MissedPunchService>();
 builder.Services.AddScoped<IWorkFromHomeRequestService, WorkFromHomeRequestService>();
 builder.Services.AddScoped<IEmployeeMasterService, EmployeeMasterService>();
 builder.Services.AddScoped<IRecruitmentService, RecruitmentService>();
+builder.Services.AddScoped<IPerformanceService, PerformanceService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
