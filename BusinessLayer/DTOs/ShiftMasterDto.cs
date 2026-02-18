83e9974a9289f2a0505f6b6ecdf78c04b66c1ac9
Kittu-Mobiles <krishna@cortracker360.com>
2026-02-18 14:33:21 +0530

    Admin Shifts Allocations

--- BusinessLayer/DTOs/ShiftMasterDto.cs
+++ BusinessLayer/DTOs/ShiftMasterDto.cs
@@ -9,16 +9,19 @@ namespace BusinessLayer.DTOs
     public class ShiftMasterDto
     {
         public int ShiftID { get; set; }
-        public string ShiftName { get; set; }
-        public TimeSpan ShiftStartTime { get; set; }
-        public TimeSpan ShiftEndTime { get; set; }
+        public string? ShiftName { get; set; }
+        public string? ShiftStartTime { get; set; }
+        public string? ShiftEndTime { get; set; }
         public int? GraceTime { get; set; }
         public bool IsActive { get; set; }
         public int CompanyID { get; set; }
         public int RegionID { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? ModifiedAt { get; set; }
         public int? ModifiedBy { get; set; }
+        public int? UserId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? RegionName { get; set; }
     }
 }
