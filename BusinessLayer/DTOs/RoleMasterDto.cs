6872ecc14492fc19795f68a06733208c6e376133
Kittu-Mobiles <krishna@cortracker360.com>
2026-02-20 12:33:22 +0530

    Role Master Dropdowns kittu

--- BusinessLayer/DTOs/RoleMasterDto.cs
+++ BusinessLayer/DTOs/RoleMasterDto.cs
@@ -11,10 +11,15 @@ namespace BusinessLayer.DTOs
         public int RoleId { get; set; }
         public string? RoleName { get; set; }
         public string? RoleDescription { get; set; }
-        public bool IsActive { get; set; }
+        public bool? IsActive { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? CreatedAt { get; set; }
         public string? ModifiedBy { get; set; }
         public DateTime? ModifiedAt { get; set; }
+        public int CompanyId { get; set; }
+        public int RegionId { get; set; }
+        public int UserId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? RegionName { get; set; }
     }
 }
