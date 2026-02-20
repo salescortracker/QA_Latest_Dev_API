12f884af1887a3ef29d20c8005a542614e600e60
Karishma0116 <karishma@cortracker360.com>
2026-02-20 17:57:46 +0530

    HoildayLIstandWeekoffadminscreens
    HoildayLIstandWeekoffadminscreens

---
+++ BusinessLayer/Interfaces/IHolidayListService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Common;
+using BusinessLayer.DTOs;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IHolidayListService
+    {
+        Task<ApiResponse<IEnumerable<HolidayListDto>>> GetAll(int userId);
+
+        Task<ApiResponse<HolidayListDto?>> GetByIdAsync(int id);
+
+        Task<ApiResponse<string>> CreateAsync(CreateUpdateHolidayListDto dto);
+
+        Task<ApiResponse<string>> UpdateAsync(CreateUpdateHolidayListDto dto);
+
+        Task<ApiResponse<string>> DeleteAsync(int id);
+    }
+}
---
+++ BusinessLayer/Interfaces/IWeekoffService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Common;
+using BusinessLayer.DTOs;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IWeekoffService
+    {
+        Task<ApiResponse<IEnumerable<WeekoffDto>>> GetAll(int userId);
+        Task<ApiResponse<WeekoffDto?>> GetByIdAsync(int id);
+        Task<ApiResponse<string>> CreateAsync(WeekoffDto dto);
+        Task<ApiResponse<string>> UpdateAsync(WeekoffDto dto);
+        Task<ApiResponse<string>> DeleteAsync(int id);
+    }
+}
